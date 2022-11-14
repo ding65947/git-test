d8062d9ac149570bc2c468630f1db48dbd09cbc5
ding <dingdi@hrbeu.edu.cn>
2022-11-14 10:50:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;/**
+ * @author DingDi
+ * @create 2022-11-14 10:46
+ */
+
+/**
+ *
+ * @author: dingdi
+ * @date: 2022/11/14 10:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
